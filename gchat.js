e0dd4cc40a6d28dcfad0ca1af6491ae766404f88
Justin Tieu <tieujustin@gmail.com>
2014-07-11 13:28:01 -0700

    versi

--- gchat.js
+++ gchat.js
@@ -2,7 +2,7 @@
 	$(document).bind('DOMSubtreeModified',function(){	
 		var a = document.getElementsByTagName("a");
 		for(var i = 0; i < a.length; i++) { 
-			if(a[i].href.indexOf(".gif") > -1 && a[i].parentNode.innerHTML.indexOf("img") == -1) { 
+			if(a[i].href.indexOf(".gif") > -1 && a[i].parentNode.innerHTML.indexOf("<img") == -1) { 
 				a[i].parentNode.innerHTML = "<a href=" + a[i].href + "><img src=" + a[i].href + " width=200/></a>";
 			} 	
 		}
