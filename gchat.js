ba44dc6696f4cfe948561dfb92be72f88924169e
Justin Tieu <tieujustin@gmail.com>
2014-07-11 13:27:37 -0700

    version 0.1.0
    Initial commit for GChat GIF(t)s

---
+++ gchat.js
@@ -0,0 +1,10 @@
+(function() {
+	$(document).bind('DOMSubtreeModified',function(){	
+		var a = document.getElementsByTagName("a");
+		for(var i = 0; i < a.length; i++) { 
+			if(a[i].href.indexOf(".gif") > -1 && a[i].parentNode.innerHTML.indexOf("img") == -1) { 
+				a[i].parentNode.innerHTML = "<a href=" + a[i].href + "><img src=" + a[i].href + " width=200/></a>";
+			} 	
+		}
+	})
+}());
