24c716048c82031d3ef0bd5eccbc86d88db6975c
Justin Tieu <tieujustin@gmail.com>
2014-07-14 14:44:20 -0700

    v0.2.0.2 - supports more image types

--- gchat.js
+++ gchat.js
@@ -1,7 +1,7 @@
 (function() {
 	$(document).bind('DOMSubtreeModified',function(){	
 		var a = document.getElementsByTagName("a");
-		var imageTypes = ['.gif', '.jpg', '.png'];
+		var imageTypes = ['.tif', '.tiff', '.gif', '.jpeg', '.jpg', '.jif', '.jfif', '.png'];
 		for(var i = 0; i < a.length; i++) { 
 			if(RegExp(imageTypes.join('|')).exec(a[i].href) && a[i].parentNode.innerHTML.indexOf("<img") == -1) { 
 				a[i].parentNode.innerHTML = "<a href=" + a[i].href + "><img src=" + a[i].href + " width=200/></a>";
