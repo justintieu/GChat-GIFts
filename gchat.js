2bde74b614c7206f9dfb1ca19cb5ebcfe8b98f2e
Justin Tieu <tieujustin@gmail.com>
2014-07-14 14:44:49 -0700

    v0.2.0.3 - updated regex

--- gchat.js
+++ gchat.js
@@ -1,9 +1,8 @@
 (function() {
 	$(document).bind('DOMSubtreeModified',function(){	
 		var a = document.getElementsByTagName("a");
-		var imageTypes = ['.tif', '.tiff', '.gif', '.jpeg', '.jpg', '.jif', '.jfif', '.png'];
 		for(var i = 0; i < a.length; i++) { 
-			if(RegExp(imageTypes.join('|')).exec(a[i].href) && a[i].parentNode.innerHTML.indexOf("<img") == -1) { 
+			if((a[i].href).match(/\.(tif|tiff|jif|jfif|jpeg|jpg|gif|png)$/) && a[i].parentNode.innerHTML.indexOf("<img") == -1) { 
 				a[i].parentNode.innerHTML = "<a href=" + a[i].href + "><img src=" + a[i].href + " width=200/></a>";
 			} 	
 		}
